882dd9d7c27a6c69cc91b0c7668187e5424d72ca
Safak <safak.kocaoglu@bil.omu.edu.tr>
2021-02-07 03:09:50 +0100

    commit

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-const item = document.querySelector("#shape");
+const list = document.querySelector(".list")
+const circle = document.querySelector(".circle")
+const lines = document.querySelector(".lines")
+const line1 = document.querySelector(".line-1")
+const line2 = document.querySelector(".line-2")
+const line3 = document.querySelector(".line-3")
 
-console.log(item.getTotalLength())
+lines.addEventListener("click",()=>{
+    list.classList.toggle("active")
+    circle.classList.toggle("active")
+    line1.classList.toggle("active")
+    line2.classList.toggle("active")
+    line3.classList.toggle("active")
+})
+
